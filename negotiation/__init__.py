58c6f36cd48e6a434bf07537b63ab6b79198e68c
lenardst <61589179+lenardst@users.noreply.github.com>
2022-12-29 02:19:18 +0100

    Basic offer logic

---
+++ negotiation/__init__.py
@@ -0,0 +1,58 @@
+from otree.api import *
+
+
+doc = """Negotiation between players"""
+
+class C(BaseConstants):
+    NAME_IN_URL = 'negotiation'
+    PLAYERS_PER_GROUP = 6
+    NUM_ROUNDS = 30
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+
+    pass
+
+
+class Player(BasePlayer):
+    exchange_partner = models.IntegerField()
+    send = models.IntegerField()
+    receive = models.IntegerField()
+    deviation = models.IntegerField()
+    deviation_partner = models.IntegerField()
+    round_net = models.IntegerField()
+    pass
+
+# PAGES
+
+
+class Negotiation(Page):
+    form_model = 'player'
+    form_fields = ['exchange_partner', 'send', 'receive']
+    timeout_seconds = 180
+
+    @staticmethod
+    def live_method(player, data):
+        return{data[0]: [player.id_in_group, data[1], data[2]]}
+
+    pass
+
+
+class WaitForExchange(WaitPage):
+    pass
+
+
+class Deviation(Page):
+    form_model = 'player'
+    form_fields = ['deviation']
+    pass
+
+
+class WaitAfterExchange(WaitPage):
+    pass
+
+
+page_sequence = ['Negotiation', 'WaitForExchange', 'Deviation', 'WaitAfterExchange']
