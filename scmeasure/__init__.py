852683e8cd97f54ff4d562c92dc24656a76b277f
lenardst <61589179+lenardst@users.noreply.github.com>
2023-01-16 14:40:15 +0100

    Sc survery added

--- scmeasure/__init__.py
+++ scmeasure/__init__.py
@@ -23,13 +23,83 @@ class Player(BasePlayer):
     gift5 = models.IntegerField(initial=0, min=0, max=10)
     gift6 = models.IntegerField(initial=0, min=0, max=10)
 
-    perception1 = models.IntegerField(initial=0)
-    perception2 = models.IntegerField(initial=0)
-    perception3 = models.IntegerField(initial=0)
-    perception4 = models.IntegerField(initial=0)
-    perception5 = models.IntegerField(initial=0)
-    perception6 = models.IntegerField(initial=0)
+    close1 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+    close2 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+    close3 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+    close4 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+    close5 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+    close6 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                 choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                          [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                          [6, 'very close']])
+
+    cohesive1 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+    cohesive2 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+    cohesive3 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+    cohesive4 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+    cohesive5 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+    cohesive6 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                    choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                             [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                             [6, 'very close']])
+
+    team1 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+    team2 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+    team3 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+    team4 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+    team5 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+    team6 = models.IntegerField(widget=widgets.RadioSelectHorizontal,
+                                choices=[[0, 'very distant'], [1, 'distant'], [2, 'rather distant'],
+                                         [3, 'neither close nor distant'], [4, 'rather close'], [5, 'close'],
+                                         [6, 'very close']])
+
     pass
+
 
 # Pages
 
@@ -53,15 +123,40 @@ class GiftGiving(Page):
         return dict(
             other_players=[p.id_in_group for p in player.get_others_in_subsession()]
         )
+
     pass
 
 
 class Questionaire(Page):
     form_model = 'player'
-    form_fields = ['perception1', 'perception2', 'perception3', 'perception4', 'perception5', 'perception6']
+
+    @staticmethod
+    def get_form_fields(player: Player):
+        form_fields = ['close1', 'cohesive1', 'team1', 'close2', 'cohesive2', 'team2',
+                   'close3', 'cohesive3', 'team3', 'close4', 'cohesive4', 'team4',
+                   'close5', 'cohesive5', 'team5', 'close6', 'cohesive6', 'team6']
+        form_fields.pop(player.id_in_group*3-3)
+        form_fields.pop(player.id_in_group * 3 - 3)
+        form_fields.pop(player.id_in_group * 3 - 3)
+        return form_fields
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            other_players=[p.id_in_group for p in player.get_others_in_subsession()],
+        )
+
+    @staticmethod
+    def js_vars(player: Player):
+        return dict(
+            other_players=[p.id_in_group for p in player.get_others_in_subsession()]
+        )
+
     pass
+
 
 class Results(Page):
     pass
+
 
 page_sequence = [GiftGiving, Questionaire, Results]
