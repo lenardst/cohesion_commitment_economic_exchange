8eedef55f47dd0f9911d1a0dbcde840a6718efef
lenardst <61589179+lenardst@users.noreply.github.com>
2023-02-21 12:58:40 +0100

    Correct round number

--- negotiation/__init__.py
+++ negotiation/__init__.py
@@ -11,7 +11,7 @@ doc = """Negotiation between players"""
 class C(BaseConstants):
     NAME_IN_URL = 'negotiation'
     PLAYERS_PER_GROUP = 6
-    NUM_ROUNDS = 25 #numpy.random.binomial(10, 0.5) + 20 + 1
+    NUM_ROUNDS = 24 #numpy.random.binomial(10, 0.5) + 20 + 1
     PAY_TRADED_UNIT = cu(0.03)
     PAY_BUDGET_UNIT = cu(0.01)
     UNIT_BUDGET = 15
